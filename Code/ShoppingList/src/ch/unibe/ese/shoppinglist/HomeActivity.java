31a4bcf8c45b93dc05ca80137a8491889de459a5
Linus Schwab <linus.schwab@students.unibe.ch>
2013-10-11 14:22:39 +0200

    Added button to create a new list

--- Code/ShoppingList/src/ch/unibe/ese/shoppinglist/HomeActivity.java
+++ Code/ShoppingList/src/ch/unibe/ese/shoppinglist/HomeActivity.java
@@ -19,6 +19,7 @@ public class HomeActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.home, menu);
+		getMenuInflater().inflate(R.menu.home_menu, menu);
 		return true;
 	}
 }
