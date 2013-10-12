28fc926b3613b7ea20e11252a23b0c93aff4ee32
orlando-signer <orlando.signer@students.unibe.ch>
2013-10-12 18:03:10 +0200

    new project for unittests

---
+++ Code/ShoppingListTests/src/ch/unibe/ese/core/ListManagerTest.java
@@ -0,0 +1,40 @@
+package ch.unibe.ese.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ListManagerTest {
+
+	private ListManager manager;
+
+	@Before
+	public void setUp() throws Exception {
+		this.manager = new ListManager();
+	}
+
+	@Test
+	public void addShoppingList() {
+		assertTrue(manager.getShoppingLists().isEmpty());
+		ShoppingList list = new ShoppingList("list1");
+		manager.addShoppingList(list);
+
+		assertEquals(1, manager.getShoppingLists().size());
+		assertEquals(list, manager.getShoppingLists().get(0));
+	}
+
+	@Test
+	public void addShoppingLists() {
+		assertTrue(manager.getShoppingLists().isEmpty());
+		ShoppingList list1 = new ShoppingList("list1");
+		ShoppingList list2 = new ShoppingList("list2");
+		manager.addShoppingList(list1);
+		manager.addShoppingList(list2);
+
+		assertEquals(2, manager.getShoppingLists().size());
+		assertEquals(list1, manager.getShoppingLists().get(0));
+		assertEquals(list2, manager.getShoppingLists().get(1));
+	}
+
+}
