a61bfcb58932568492a8443ebd7a36c027dfab62
Linus Schwab <linus.schwab@students.unibe.ch>
2013-10-14 20:25:42 +0200

    Updated CreateListActivity layout

--- Code/ShoppingList/src/ch/unibe/ese/shoppinglist/CreateListActivity.java
+++ Code/ShoppingList/src/ch/unibe/ese/shoppinglist/CreateListActivity.java
@@ -2,7 +2,9 @@ package ch.unibe.ese.shoppinglist;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class CreateListActivity extends Activity {
 
@@ -10,6 +12,7 @@ public class CreateListActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_list);
+		// hide the action bar on this activity
 		getActionBar().hide();
 	}
 
@@ -18,6 +21,12 @@ public class CreateListActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.create_list, menu);
 		return true;
+	}
+	
+	/** Called when the user touches the abort button */
+	public void goBack(View view) {
+    	Intent intent = new Intent(this, HomeActivity.class);
+        this.startActivity(intent);
 	}
 
 }
