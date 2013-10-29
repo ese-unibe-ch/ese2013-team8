eba78eec5db42b676b1763d0f6b78046b8d8bffb
Pascal Giehl <p.giehl@gmx.ch>
2013-10-29 16:05:54 +0100

    Some features to add friends, no sql till now

--- Code/ShoppingList/src/ch/unibe/ese/activities/ManageFriendsActivity.java
+++ Code/ShoppingList/src/ch/unibe/ese/activities/ManageFriendsActivity.java
@@ -1,7 +1,5 @@
 package ch.unibe.ese.activities;
 
-import java.util.List;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -16,21 +14,18 @@ import ch.unibe.ese.shoppinglist.R;
 
 public class ManageFriendsActivity extends BaseActivity {
 	FriendsManager friendsManager;
-	List<Friend> friendsList;
 
 	@Override
+	/**
+	 * initializes the friendslist and loads all friends.
+	 */
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_manage_friends);
 
 		friendsManager = getFriendsManager();
-		friendsList = friendsManager.getFriendsList();
 
-		ArrayAdapter<Friend> friendsAdapter = new ArrayAdapter<Friend>(this,
-				R.layout.shopping_list_item, friendsList);
-
-		ListView listView = (ListView) findViewById(R.id.friends_list);
-		listView.setAdapter(friendsAdapter);
+		updateFriendsList();
 
 	}
 
@@ -39,27 +34,56 @@ public class ManageFriendsActivity extends BaseActivity {
 		getMenuInflater().inflate(R.menu.manage_friends, menu);
 		return true;
 	}
-
-	public void save(View w) {
-		EditText friendName = (EditText) findViewById(R.id.friends_name_edit);
-		String name = friendName.getText().toString();
-
-		EditText friendNr = (EditText) findViewById(R.id.friends_nr_edit);
-		int nr = Integer.parseInt(friendNr.getText().toString());
-
-		if (!friendsManager.addFriend(nr, name))
-			Toast.makeText(this, this.getString(R.string.error_friend), Toast.LENGTH_SHORT).show();
-
-		friendName.setText("");
-		friendNr.setText("");
-
-		// TODO: durch schoenere Variante ersetzen
-		friendsList = friendsManager.getFriendsList();
+	
+	/**
+	 * Updates the Viewlist, which shows all friends
+	 */
+	public void updateFriendsList(){
 		ArrayAdapter<Friend> friendsAdapter = new ArrayAdapter<Friend>(this,
-				R.layout.shopping_list_item, friendsList);
+				R.layout.shopping_list_item, friendsManager.getFriendsList());
 
 		ListView listView = (ListView) findViewById(R.id.friends_list);
-		listView.setAdapter(friendsAdapter);
+		listView.setAdapter(friendsAdapter);	
+	}
 
+	/**
+	 * Reads the input and tries to creates a friend with it
+	 * @param View v
+	 */
+	public void addEntryToList(View w) {
+		try{
+			EditText friendName = (EditText) findViewById(R.id.friends_name_edit);
+			String name = friendName.getText().toString();
+
+			EditText friendNr = (EditText) findViewById(R.id.friends_nr_edit);
+			int nr = Integer.parseInt(friendNr.getText().toString());
+	
+			int processStatus = friendsManager.addFriend(nr, name);
+			if (processStatus == 0){
+				friendName.setText("");
+				friendNr.setText("");
+	
+				updateFriendsList();
+			} else 
+				printFailure(processStatus);
+		} catch(Exception e){
+			Toast.makeText(this, this.getString(R.string.error_enter), Toast.LENGTH_SHORT).show();
+		}
+	}
+	
+	/**
+	 * prints the failure created by adding a friend
+	 * @param failureNumber
+	 */
+	private void printFailure(int failure){
+		switch(failure){
+		case 1: 
+			Toast.makeText(this, this.getString(R.string.error_friend_already), Toast.LENGTH_SHORT).show();
+			break;
+			
+		case 2: 
+			Toast.makeText(this, this.getString(R.string.error_name), Toast.LENGTH_SHORT).show();
+			break;
+		}
 	}
 }
