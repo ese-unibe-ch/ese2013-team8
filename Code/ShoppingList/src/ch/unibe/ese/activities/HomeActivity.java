6fd87286693c9e258423295ddfc253a540d7b5da
Linus Schwab <linus.schwab@students.unibe.ch>
2013-10-31 20:25:41 +0100

    Added drawer menu button

--- Code/ShoppingList/src/ch/unibe/ese/activities/HomeActivity.java
+++ Code/ShoppingList/src/ch/unibe/ese/activities/HomeActivity.java
@@ -2,10 +2,13 @@ package ch.unibe.ese.activities;
 
 import java.util.List;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.telephony.TelephonyManager;
 import android.view.Menu;
@@ -34,16 +37,46 @@ public class HomeActivity extends BaseActivity {
 	private Activity homeActivity = this;
 
 	private DrawerLayout drawMenu;
+	private ActionBarDrawerToggle drawerToggle;
 
+	@SuppressLint("NewApi") // TODO: check "problem"
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
 
-		// create slide list
+		// Create drawer menu
 		NavigationDrawer nDrawer = new NavigationDrawer();
 		drawMenu = nDrawer.constructNavigationDrawer(drawMenu, this);
+		
+		drawerToggle = new ActionBarDrawerToggle(
+                this,                  	/* host Activity */
+                drawMenu,         		/* DrawerLayout object */
+                R.drawable.ic_drawer,  	/* nav drawer icon to replace 'Up' caret */
+                R.string.drawer_open,  	/* "open drawer" description */
+                R.string.drawer_close  	/* "close drawer" description */
+                ) {
 
+			// At the moment the following two methods are useless, but maybe 
+			// we will want to add stuff later
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(R.string.app_name);
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(R.string.app_name);
+            }
+        };
+        
+        // Set the drawer toggle as the DrawerListener
+        drawMenu.setDrawerListener(drawerToggle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        
 		listmanager = getListManager();
 		syncmanager = getSyncManager();
 
@@ -86,6 +119,19 @@ public class HomeActivity extends BaseActivity {
 		});
 
 	}
+	
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+    
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -96,6 +142,12 @@ public class HomeActivity extends BaseActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (drawerToggle.onOptionsItemSelected(item)) {
+          return true;
+        }
+        
 		// Handle presses on the action bar items
 		switch (item.getItemId()) {
 
