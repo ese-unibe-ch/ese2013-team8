d4a353034280f834165490c182bc10217f8b23ec
Pascal Giehl <p.giehl@gmx.ch>
2013-11-11 21:13:15 +0100

    Add friends from phone book

--- Code/ShoppingList/src/ch/unibe/ese/shopnote/activities/ManageFriendsActivity.java
+++ Code/ShoppingList/src/ch/unibe/ese/shopnote/activities/ManageFriendsActivity.java
@@ -1,7 +1,11 @@
 package ch.unibe.ese.shopnote.activities;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.NavUtils;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
@@ -11,6 +15,8 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+import ch.unibe.ese.shopnote.R;
 import ch.unibe.ese.shopnote.adapters.FriendsListAdapter;
 import ch.unibe.ese.shopnote.core.BaseActivity;
 import ch.unibe.ese.shopnote.core.Friend;
@@ -18,7 +24,6 @@ import ch.unibe.ese.shopnote.core.FriendsManager;
 import ch.unibe.ese.shopnote.drawer.NavigationDrawer;
 import ch.unibe.ese.shopnote.share.SyncManager;
 import ch.unibe.ese.shopnote.share.requests.FriendRequest;
-import ch.unibe.ese.shopnote.R;
 
 /**
  * Creates a frame which enlists all friends and the possibility to manage them
@@ -27,6 +32,7 @@ import ch.unibe.ese.shopnote.R;
  */
 public class ManageFriendsActivity extends BaseActivity {
 	
+	private static final int PICK_CONTACT = 2;
 	private FriendsManager friendsManager;
 	private SyncManager syncManager;
 	private ArrayAdapter<Friend> friendsAdapter;
@@ -112,13 +118,17 @@ public class ManageFriendsActivity extends BaseActivity {
 			NavUtils.navigateUpFromSameTask(this);
 			return true;
 		case R.id.action_new:
-			Intent intent = new Intent(this, CreateFriendActivity.class);
-			this.startActivity(intent);
+//			Intent intent = new Intent(this, CreateFriendActivity.class);
+//			this.startActivity(intent);
+			Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+            startActivityForResult(intent, PICK_CONTACT);
+			
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
 		}
 	}
+	
 	
 	@Override
 	protected void onPause() {
@@ -129,6 +139,34 @@ public class ManageFriendsActivity extends BaseActivity {
 	
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
+		
+		if (resultCode == Activity.RESULT_OK && requestCode == PICK_CONTACT) 
+				addChosenFriend(data);
+	}
+
+	/**
+	 * Extracts the Contact from data and adds it to the db
+	 * @param data
+	 */
+	private void addChosenFriend(Intent data) {
+		Uri contactData = data.getData();
+		Cursor cursor = managedQuery(contactData, null, null, null, null);
+	     
+		 if (cursor.moveToFirst()) {
+			  String id = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.Contacts._ID));
+			  String hasPhone = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+	
+			  if (hasPhone.equalsIgnoreCase("1")) {
+				  Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null, 
+						  ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = "+ id,null, null);
+				  phones.moveToFirst();
+				  String cNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+				  String cName = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+				  
+				Friend friend = new Friend(cNumber, cName);
+				friendsManager.addFriend(friend);
+			  }	
+	     }
 	}
 
 	@Override
